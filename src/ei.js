6ed4f8a571997a1bee23d511669b8e7967274d20
rockerest <websites@thomasrandolph.info>
2014-06-15 23:32:48 -0500

    Additions and improvements
    Ensure unique identifiers at expense of speed
    Load polyfills for QuerySelectorAll and RequestAnimationFrame
    Use Events

--- src/ei.js
+++ src/ei.js
@@ -1,28 +1,53 @@
 define(
-	["init"],
-	function( Init ){
+	["require", "underscore", "init", "events/init", "polyfills/animation", "polyfills/qsa"],
+	function( require, _, Init, Events ){
 		var ei = function(){
-			this.canvasses = [];
-
-			console.info( "Eijs started." );
-		};
+				this.canvasses = {};
+				this.events = new Events( this );
+			},
+			store;
 
 		ei.prototype.manage = function( node ){
-			var loaded = Init.load( node );
-			if( loaded ){
-				this.canvasses.push( loaded );
-			}
-
-			return loaded;
+			return store( this, node );
 		};
 
 		ei.prototype.spawn = function( w, h ){
 			var canvas = Init.create();
 			canvas.width = w;
 			canvas.height = h;
-			this.canvasses.push( canvas );
 
-			return canvas;
+			return store( this, canvas );
+		};
+
+		ei.prototype.getCanvas = function( id ){
+			if( id && this.canvasses[ id ] ){
+				return this.canvasses[ id ];
+			}
+			else{
+				return this.canvasses;
+			}
+		};
+
+		store = function( Ei, node ){
+			var stored = false,
+				loaded, id;
+
+			while( !stored ){
+				if( loaded ){
+					id = loaded.getAttribute( "data-eijs-id" );
+					if( !(_(Ei.canvasses).has( id )) ){
+						Ei.canvasses[ id ] = loaded;
+						stored = true;
+					}
+				}
+				else{
+					loaded = Init.load( node );
+				}
+			}
+
+			Ei.events.fire( "eijs.init/canvas/manage" );
+
+			return loaded;
 		};
 
 		return ei;
