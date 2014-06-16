534bd44757e3022e7d7c2b6a80028bdf998553be
rockerest <websites@thomasrandolph.info>
2014-06-15 23:33:05 -0500

    Add Polyfills

---
+++ src/polyfills/qsa.js
@@ -0,0 +1,16 @@
+define(function(){
+    if( !document.querySelectorAll){
+        document.querySelectorAll = function( selector ){
+            var doc = document,
+            head = doc.documentElement.firstChild,
+            styleTag = doc.createElement('STYLE');
+            head.appendChild(styleTag);
+            doc.__qsaels = [];
+
+            styleTag.styleSheet.cssText = selector + "{x:expression(document.__qsaels.push(this))}";
+            window.scrollBy(0, 0);
+
+            return doc.__qsaels;
+        };
+    }
+});
