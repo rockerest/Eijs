5020b1077c9012994423f47ba5d4fa63b11e32bd
rockerest <websites@thomasrandolph.info>
2014-06-24 23:39:45 -0500

    Using Spec as default reporter cause Nyan isn't verbose enough :(

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,7 +47,7 @@ module.exports = function( grunt ){
                 "src": "test/index.html",
                 "options": {
                     "growlOnSuccess": true,
-                    "reporter": "Nyan"
+                    "reporter": "Spec"
                 }
             }
         },
