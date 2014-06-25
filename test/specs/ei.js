6f281c74a7708637d037f8345001f9fa94e97d55
rockerest <websites@thomasrandolph.info>
2014-06-24 23:39:26 -0500

    Cleaned up test

--- test/specs/ei.js
+++ test/specs/ei.js
@@ -2,8 +2,17 @@ define(
     [ "ei" ],
     function( Ei ){
         describe( "Ei", function(){
-            it( "is an object", function(){
+            it( "is a constructor", function(){
                 Ei.should.be.a( "function" );
+            });
+
+            describe( "when constructed", function(){
+                it( "should construct an object", function(){
+                    var result = new Ei();
+
+                    result.should.be.a( "object" );
+                    result.manage.should.be.a( "function" );
+                });
             });
         });
     }
